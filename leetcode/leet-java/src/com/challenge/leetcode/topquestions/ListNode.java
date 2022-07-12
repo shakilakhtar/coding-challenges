90b208681066690f6f6ec31ed2f06fb1166c7428
Tech Panda <10shakil.akhtar@gmail.com>
2022-07-12 08:20:09 +0530

    added new solution

--- leetcode/leet-java/src/com/challenge/leetcode/topquestions/ListNode.java
+++ leetcode/leet-java/src/com/challenge/leetcode/topquestions/ListNode.java
@@ -8,5 +8,5 @@ package com.challenge.leetcode.topquestions;
 public class ListNode {
      public int val;
      public ListNode next;
-     ListNode(int x) { val = x; }
+     public ListNode(int x) { val = x; }
 }
